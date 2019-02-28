460de566b279970d647ae665160f952db25fb835
Ricky Bernal <rickyb@rickys-mbp.attlocal.net>
2019-02-28 00:20:52 -0800

    Finished part 2 of the Flix project without the bonus objectives

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Ricky Bernal on 2/27/19.
+//  Copyright © 2019 Ricky Bernal. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
